4cc1f31ade24fdecb00f1721121bd4c6f8ed92c8
Joao Borba <joao3borba3@gmail.com>
2022-03-28 17:20:44 -0300

    Projeto 1/2

--- Projeto 1 de 2/src/project1/MyFrame.java
+++ Projeto 1 de 2/src/project1/MyFrame.java
@@ -2,36 +2,46 @@ package project1;
 
 import javax.swing.*;
 
+import java.awt.*;
+
+import static java.awt.Toolkit.*;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 public class MyFrame extends JFrame {
 
     //Criando um panel para o frame
     MyPanel panel;
+    //ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("icone.png"));
+    ImageIcon icone = new ImageIcon(getDefaultToolkit().getImage(getClass().getResource("logo.png")));
 
     //Metodo construtor
     public MyFrame() {
-        showMessageDialog(this, "Instruções de Uso:\n(Aperte H para abrir esta janela a qualquer momento):\n" +
-                "\nPressionar as teclas:\n" +
-                "\n" +
-                "\"E\" para criar uma elipse\n" +
-                "\n" +
-                "\"R\" para criar um retangulo\n" +
-                "\n" +
-                "\"DELETE\" para deletar a figura selecionada\n" +
-                "\n" +
-                "\"F\" para mudar a cor de fundo da figura selecionada. Caso nao tenha nenhuma figura em foco, mudara a cor da tela toda \n" +
-                "\n" +
-                "\"C\" para mudar a cor de contorno da figura selecionada\n" +
-                "\n" +
-                "\"Pressione e arraste a borda inferior direita da figura ou segure SHIFT e use as setas do teclado\" para mudar o tamanho da figura selecionada\n" +
-                "\n" +
-                "OBS: PARA SELECIONAR UMA FIGURA, BASTA CLICAR COM O MOUSE EM CIMA DELA");
+        this.setIconImage(icone.getImage());
+
+        showMessageDialog(this, """
+                Instruções de Uso:
+                (Aperte H para abrir esta janela a qualquer momento):
+
+                Pressionar as teclas:
+
+                "E" para criar uma elipse
+
+                "R" para criar um retangulo
+
+                "DELETE" para deletar a figura selecionada
+
+                "F" para mudar a cor de fundo da figura selecionada. Caso nao tenha nenhuma figura em foco, mudara a cor da tela toda\s
+
+                "C" para mudar a cor de contorno da figura selecionada
+
+                "Pressione e arraste a borda inferior direita da figura ou segure SHIFT e use as setas do teclado" para mudar o tamanho da figura selecionada
+
+                OBS: PARA SELECIONAR UMA FIGURA, BASTA CLICAR COM O MOUSE EM CIMA DELA""");
         //Inicializando o panel
         panel = new MyPanel();
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE); //Fazendo o programa fechar ao fechar o frame
-        this.setTitle("Frame"); //Mudando o título do frame
+        this.setTitle("Borba's Vector Graphic Editor"); //Mudando o título do frame
         this.add(panel); //Adicionando o panel no frame
         this.pack(); //Fazendo o frame ter o mesmo tamanho que o panel
         this.setLocationRelativeTo(null); //Fazendo o frame inicializar no meio da tela
